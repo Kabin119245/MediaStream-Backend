bcae42eece09fa2ec80abf1a36a0b7e37dc0d0c2
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-10-17 18:30:31 +0545

    utils code implemented

--- src/index.js
+++ src/index.js
@@ -7,4 +7,12 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server started on port:  ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed: " + err);
+  });
