45ebad59f05cd64dfebb3a5a811ba356b681604a
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-10-27 21:49:08 +0545

    add getChannel Controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -311,6 +311,53 @@ const updateUserCover = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "User cover image has been updated"));
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+
+  if (!username?.trim()) {
+    throw new ApiError(400, "Username is required");
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: { username: username?.toLowerCase() },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscriberdTo",
+      },
+    },
+    {
+      $addFields: {
+        subscriberCount: {
+          $size: "$subscribers",
+        },
+        channelsSubscribedToCount: {
+          $size: "$subscriberdTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+  ]);
+});
+
 export {
   registerUser,
   loginUser,
